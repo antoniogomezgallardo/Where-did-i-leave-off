1a6b14acf5b087f52659b86f63c34c7ad93e4119
Tony <antoniogomezgallardo@gmail.com>
2022-07-28 23:35:48 +0200

    Add comments to clarify the code

--- background.js
+++ background.js
@@ -1,7 +1,11 @@
 
-// background.js
+/*
+This extension will need information from a persistent variable as soon as it's installed. Start by including a listening event for runtime.onInstalled in the background script. Inside the onInstalled listener, the extension will set a value using the storage API. This will allow multiple extension components to access that value and update it. Inside the extension's directory create a file named background.js and add the following code.
 
-let color = '#3aa757';
+Most APIs, including the storage API, must be registered under the "permissions" field in the manifest for the extension to use them.
+*/
+
+let color = '#3aa757'; // This is the variable that holds the color to which the background is gonna change
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({color});
--- popup.js
+++ popup.js
@@ -2,13 +2,22 @@
 
 let changeColor = document.getElementById("changeColor");
 
+// This code grabs the button from popup.html and requests the color value from storage. It then applies the color as the background of the button. Just the button, not the background of the page.
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  changeColor.style.backgroundColor = color; 
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
+/*
+When the button is clicked, inject setPageBackgroundColor into current page
+
+The updated code adds a click event listener to the button, which triggers a programmatically injected content script. This turns the background color of the page the same color as the button. Using programmatic injection allows for user-invoked content scripts, instead of auto inserting unwanted code into web pages.
+
+The manifest will need the activeTab permission to allow the extension temporary access to the current page, and the scripting permission to use the Scripting API's executeScript method.
+*/
+
 changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
